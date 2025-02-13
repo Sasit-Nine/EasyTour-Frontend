03818b6922572ec4790606836b34e560969179b8
Pratya <pratchaya.jin@gmail.com>
2025-02-13 23:25:36 +0700

    Push before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import CustomerApp from './customers-side/CustomerApp'
+import AdminApp from './admin-side/AdminApp'
 
 function App() {
   return (
     <>
-      <CustomerApp></CustomerApp>
+      <AdminApp></AdminApp> 
     </>
   )
 }
 
 export default App;
+  
