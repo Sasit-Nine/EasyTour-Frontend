69f6ba11c1ba33adb3c41189e94da5346ca72346
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-02-14 00:17:54 +0700

    Tailwind Integrate and UI Design Unsuccess

--- src/customers-side/subpage/Home.jsx
+++ src/customers-side/subpage/Home.jsx
@@ -1,56 +1,8 @@
 import React from "react"
-import "../../css/Home.scss";
+import Welcome from "../components/Welcome"
 const Home = () => {
     return (
-        <div className='contianer'>
-            <div className='img'>
-                <h1 className='text-quotation'>EXPLORE THE WHOLE WORLD AND ENJOY ITS BEAUTY</h1>
-            </div>
-
-            {/* ส่วนล่าง toptravel */}
-
-            <div className='toptravel'>
-                <h2>TOPTRAVEL</h2>
-                <div className='cardlayer'>
-                    <div className='cardtour'>
-                        <div className='imgtour'>
-                            <img src='https://img.kapook.com/u/2018/sutasinee/05/p1_1.jpg'/>
-                        </div>
-                        <div className='texttour'>
-                            <div className='text'>เกาะหลีเป๊ะ จ.สตูล</div>
-                            <button>seemore</button>
-                        </div>
-                    </div>
-                    <div className='cardtour'>
-                        <div className='imgtour'>
-                            <img src='https://img.kapook.com/u/2018/sutasinee/05/p1_1.jpg'/>
-                        </div>
-                        <div className='texttour'>
-                            <div className='text'>เกาะหลีเป๊ะ จ.สตูล</div>
-                            <button>seemore</button>
-                        </div>
-                    </div>
-                    <div className='cardtour'>
-                        <div className='imgtour'>
-                            <img src='https://img.kapook.com/u/2018/sutasinee/05/p1_1.jpg'/>
-                        </div>
-                        <div className='texttour'>
-                            <div className='text'>เกาะหลีเป๊ะ จ.สตูล</div>
-                            <button>seemore</button>
-                        </div>
-                    </div>
-                    <div className='cardtour'>
-                        <div className='imgtour'>
-                            <img src='https://img.kapook.com/u/2018/sutasinee/05/p1_1.jpg'/>
-                        </div>
-                        <div className='texttour'>
-                            <div className='text'>เกาะหลีเป๊ะ จ.สตูล</div>
-                            <button>seemore</button>
-                        </div>
-                    </div>
-                </div></div>
-
-        </div>
+        <Welcome></Welcome>
     )
 }
 export default Home
--- src/customers-side/subpage/PackageList.jsx
+++ src/customers-side/subpage/PackageList.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { QUERY_PACKAGELIST } from "../../services/Graphql";
 import { useNavigate } from "react-router-dom";
-import {Card,Col,Row,Spin} from 'antd'
-import '../../css/PackageList.scss'
 const PackageList = () => {
     const navigate = useNavigate()
     const strapiBaseURL = import.meta.env.VITE_STRAPI_URL
@@ -44,38 +42,66 @@ const PackageList = () => {
 
     console.log(transformedPackages)
     return (
-        <div>
-          <div className="PackageList">
-            <Row gutter={16}>
-                {transformedPackages.map((pkg)=>(
-                    <Col key={pkg.documentId} xs={24} sm={12} md={8} lg={6} xl={4}>
-                        <Card
-                            className="card"
-                            hoverable
-                            cover={<img src={`${strapiBaseURL}${pkg.url}`} />}
-                            onClick={()=> handleToPackageDetail(pkg.documentId)}
-                        >
-                            <Card.Meta
-                                title={pkg.name}
-                                description={
-                                    <div>
-                                        <p>Price : ฿ {pkg.price}</p>
-                                        <p>Duration : {pkg.duration}</p>
-                                        <p>Location : {pkg.location}</p>
-                                        <p>
-                                            {pkg.start} - {pkg.end}
-                                        </p>
-                                    </div>
-                                }
-                            >
+        <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">แพ็คเกจท่องเที่ยว</h2>
 
-                            </Card.Meta>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-          </div>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {transformedPackages.map((product) => (
+            <div key={product.documentId} className="group relative cursor-pointer" onClick={()=>handleToPackageDetail(product.documentId)} >
+              <img
+                src={`${strapiBaseURL}${product.url}`}
+                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+              />
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.location}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
+    </div>
+        // <div>
+        //   <div className="PackageList">
+        //     <Row gutter={16}>
+        //         {transformedPackages.map((pkg)=>(
+        //             <Col key={pkg.documentId} xs={24} sm={12} md={8} lg={6} xl={4}>
+        //                 <Card
+        //                     className="card"
+        //                     hoverable
+        //                     cover={<img src={`${strapiBaseURL}${pkg.url}`} />}
+        //                     onClick={()=> handleToPackageDetail(pkg.documentId)}
+        //                 >
+        //                     <Card.Meta
+        //                         title={pkg.name}
+        //                         description={
+        //                             <div>
+        //                                 <p>Price : ฿ {pkg.price}</p>
+        //                                 <p>Duration : {pkg.duration}</p>
+        //                                 <p>Location : {pkg.location}</p>
+        //                                 <p>
+        //                                     {pkg.start} - {pkg.end}
+        //                                 </p>
+        //                             </div>
+        //                         }
+        //                     >
+
+        //                     </Card.Meta>
+        //                 </Card>
+        //             </Col>
+        //         ))}
+        //     </Row>
+        //   </div>
+        // </div>
     )
 };
 
